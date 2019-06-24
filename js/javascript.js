74a1cce993679c4079d5ba16a33171c37f6ed878
giuliaahid <gxahid234@gmail.com>
2019-06-24 12:47:32 -0300

    "Algumas alterações no script e html"

--- js/javascript.js
+++ js/javascript.js
@@ -28,3 +28,37 @@ for (var i = 0; pacientes.length > i; i++) {
     pacientes[i].classList.add('linha-erro');
   }
 }
+
+var botaoAdicionar = document.querySelector ('#adicionar-paciente');
+botaoAdicionar.addEventListener ('click', mostrarAlerta);
+
+function mostrarAlerta (evento){
+evento.preventDefault ();
+alert ('Clique Aqui');
+
+var formulario = document.querySelector ('#formulario');
+var nome = formulario.nome.value;
+var peso = formulario.peso.value;
+var altura = formulario.altura.value;
+var gordura = formulario.gordura.value;
+
+var pacienteTr = document.createElement ("tr");
+var nomeTd = document.createElement ("td"); 
+var pesoTd = document.createElement ("td"); 
+var alturaTd = document.createElement ("td"); 
+var gorduraTd = document.createElement ("td"); 
+
+
+nomeTd.textContent = nome;
+pesoTd.textContent = peso;
+alturaTd.textContent = altura;
+gorduraTd.textContent = gordura;
+
+pacienteTr.appendChild (nomeTd);
+pacienteTr.appendChild (pesoTd);
+pacienteTr.appendChild (alturaTd);
+pacienteTr.appendChild (gorduraTd);
+
+var table = document.querySelector ('#tabela-paciente);
+table.appendChild (pacienteTr);
+}
