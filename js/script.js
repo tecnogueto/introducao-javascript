d8a8c2d5957eb53a6d8639c7c3df0d10cbc652bf
Rodrigo Ribeiro <digoribeiro@MacBook-Pro-de-Rodrigo.local>
2019-06-24 20:59:17 -0300

    aula 01

---
+++ js/script.js
@@ -0,0 +1,37 @@
+var titulo = document.querySelector('.titulo');
+titulo.textContent = 'Dr Digão';
+
+var pacientes = document.querySelectorAll('.paciente');
+
+for (var i = 0; i < pacientes.length; i++) {
+  var paciente = pacientes[i];
+
+  var tdPeso = paciente.querySelector('.info-peso');
+  var peso = tdPeso.textContent;
+
+  var tdAltura = paciente.querySelector('.info-altura');
+  var altura = tdAltura.textContent;
+
+  var tdImc = paciente.querySelector('.info-imc');
+
+  var imc = peso / (altura * altura);
+
+  var validarPeso = true;
+  var validarAltura = true;
+
+  if (peso <= 0 || peso >= 700) {
+    validarPeso = false;
+    tdImc.textContent = 'Peso inválido!';
+    paciente.classList.add('tr-error');
+  }
+
+  if (altura <= 0 || altura >= 3.0) {
+    validarAltura = false;
+    tdImc.textContent = 'Altura inválida!';
+    paciente.classList.add('tr-error');
+  }
+
+  if (validarAltura && validarPeso) {
+    tdImc.textContent = imc.toFixed(2);
+  }
+}
