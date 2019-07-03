58b09dd24f768f24277143037ac23a384dc823ea
Rodrigo Ribeiro <digoribeiro@MacBook-Pro-de-Rodrigo.local>
2019-07-03 13:10:16 -0300

    aula

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,7 +1,23 @@
-var pacientes = document.querySelectorAll('.paciente');
+/* var pacientes = document.querySelectorAll('.paciente');
 
 pacientes.forEach(function(paciente) {
   paciente.addEventListener('dblclick', function() {
     paciente.remove();
   });
+});
+*/
+
+var tabela = document.querySelector('#tabela-paciente');
+
+tabela.addEventListener('dblclick', function(event) {
+  var alvoEvento = event.target;
+  var paiDoAlvo = alvoEvento.parentNode;
+  var nomeCliente = alvoEvento.textContent;
+
+  paiDoAlvo.classList.add('animacao');
+
+  setTimeout(function() {
+    paiDoAlvo.remove();
+    alert('Você removeu o ' + nomeCliente);
+  }, 500);
 });
