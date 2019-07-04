626ecbd1b0271ac42e381c562ffb14434b43d7f2
viniciospr10 <viniciospr10@gmail.com>
2019-07-03 21:05:38 -0300

    ordenamos 50%

--- js/formulario.js
+++ js/formulario.js
@@ -36,3 +36,28 @@ function adicionarPaciente(evento) {
   pacienteTr.appendChild(gorduraTd);
   pacienteTr.appendChild(imcTd);
 }
+ var ordem = ["João", "Paulo", "Douglas", "Erica", "tatiana"];
+ordem.sort();
+console.log(ordem); 
+
+var pesoss = [100, 80, 54, 85, 46];
+pesoss.sort(function(a,b ){return(b-a)} );
+console.log(pesoss);
+
+var altura = [2.00, 1.72, 1.64, 1.73, 1.65];
+pesoss.sort(function(c,d){return(c-d)} );
+console.log(altura);
+
+var linha1 = ["paulo", 100]
+var linha2 = "eu";
+ordem = [];
+ordem = [linha1, linha2]
+ordem.sort()
+
+var x = [
+  ["paulo", 24],
+  ["Joao", 20]
+];
+
+x.sort()
+console.log(x)
